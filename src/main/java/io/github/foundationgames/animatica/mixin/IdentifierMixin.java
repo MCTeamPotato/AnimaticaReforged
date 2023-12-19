a52528476de312a6bcf6d2724fd77dfb259f7cd9
Kasualix <670067575@qq.com>
2023-12-20 01:09:12 +0800

    sync

--- src/main/java/io/github/foundationgames/animatica/mixin/IdentifierMixin.java
+++ src/main/java/io/github/foundationgames/animatica/mixin/IdentifierMixin.java
@@ -24,33 +24,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// Allows invalid characters in paths to support packs with extremely outdated formatting (because OptiFine does too)
-// animatica$reportInvalidIdentifierCharacters and animatica$isPathAllowed are very expensive during Identifier initialization, so they're disabled
-@SuppressWarnings("CommentedOutCode")
 @Mixin(Identifier.class)
 public class IdentifierMixin {
-    /*@Inject(method = "<init>([Ljava/lang/String;)V", at = @At("TAIL"))
-    private void animatica$reportInvalidIdentifierCharacters(String[] id, CallbackInfo ci) {
-        if (Flags.ALLOW_INVALID_ID_CHARS && !animatica$isPathAllowed(id[1]) && !id[1].startsWith("~/")) {
-            Animatica.LOG.warn("Legacy resource pack is using an invalid namespaced identifier '{}:{}'! DO NOT use non [a-z0-9_.-] characters for resource pack files and file names!", id[0], id[1]);
-        }
-    }*/
-
-    @Inject(method = "isPathCharacterValid", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "isPathCharacterValid", at = @At("HEAD"), cancellable = true)
     private static void animatica$allowInvalidCharacters(char character, CallbackInfoReturnable<Boolean> cir) {
-        if (Animatica.ALLOW_INVALID_ID_CHARS) {
-            cir.setReturnValue(true);
-        }
+        if (Animatica.ALLOW_INVALID_ID_CHARS) cir.setReturnValue(true);
     }
-
-   /* @Unique
-    private static boolean animatica$isPathAllowed(String path) {
-        if (path == null) return true;
-        for (char c : path.toCharArray()) {
-            if (!(c == '_' || c == '-' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9' || c == '/' || c == '.')) {
-                return false;
-            }
-        }
-        return true;
-    }*/
 }
